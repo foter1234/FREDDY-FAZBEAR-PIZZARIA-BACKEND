ed833f28edcf30a5f017aa9df9968dcceb0ee8ac
Lucas Pereira Esteves <lucaspesteves2017@gmail.com>
2023-08-08 08:49:29 -0400

    UPANDO BACK-END

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      connectionString: 'postgres://oxslxeuo:vIpAv8u3WJL7wWVQugQiv5v0La4bDPLX@motty.db.elephantsql.com/oxslxeuo'
+    });
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect };
---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const path = require('path');
+const Produto = require("./models/produto");
+const cors= require('cors');
+const app = express();
+app.use(cors());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/produtos', async function(req, res){
+  try {
+    var produtos = await Produto.select();
+    res.json(produtos.rows);
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao buscar produtos' });
+  }
+});
+
+app.post('/produtos', async function(req,res){
+  
+  try{
+    console.log("a")
+    var produto = req.body
+    var produto = await Produto.insert(produto);
+    console.log("b")
+    res.json(produto.rows)
+  }catch(error){
+    console.log("error")
+  }
+  
+  try{
+    var produtos = await  Produto.selectOne(req.body.id)
+    res.json(produtos.rows[0])
+  }catch(error){
+    console.error('erro ao buscar produtos', error)
+    res.status(500).json({error:'Ocorreu um erro ao buscar produtos'})
+  }
+})
+
+app.put('/produtos', async function(req, res){
+  try {
+    var produto = await Produto.update(req.body.id, req.body);
+    res.json(produto.rows);
+  } catch (error) {
+    console.error('Erro ao atualizar produto:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao atualizar produto' });
+  }
+});
+
+
+app.delete('/produtos', async function(req, res){
+  try {
+    console.log(req.body.id)
+    var produto = await Produto.delete(req.body.id);
+    res.json(produto.rows);
+  } catch (error) {
+    console.error('Erro ao atualizar pessoa:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao atualizar produtos' });
+  }
+});
+
+
+app.listen(3003, function() {
+  console.log(`app de Exemplo escutando na porta! ${3003}`)
+});
